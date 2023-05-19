5cf4552f9c1dc3e143aef4df0144216b959fdc34
Leonardo Silva Gigliotti <leonardosilvagigliottii@gmail.com>
2023-05-18 22:13:12 -0300

    Criando o login e o registro do etecbook
    Criando o login e o registro do etecbook

---
+++ EtecBookUI/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
